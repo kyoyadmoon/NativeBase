47d8b48949b037ecb185e93ec640ed6baf6b7bcd
dmoon <dmoon.t@gmail.com>
2016-11-08 02:44:56 +0800

    add style props of Picker

--- Components/Widgets/Picker.ios.js
+++ Components/Widgets/Picker.ios.js
@@ -47,6 +47,9 @@ export default class PickerNB extends NativeBaseComponent {
             pickerItem: {
 
             }
+            itemTextStyle: {
+                color: this.props.itemTextColor,
+            },
         }
     }
     _setModalVisible(visible) {
@@ -103,23 +106,23 @@ export default class PickerNB extends NativeBaseComponent {
                 textStyle={this.props.textStyle}
                 style={this.props.style}
                 onPress={() => {this._setModalVisible(true)}}>
-                {this.state.currentLabel}
-                {(this.props.iosIcon == undefined) ? <Icon name="ios-home" style={{opacity: 0}} /> : this.renderIcon()}
+                {this.state.current}
+                {(this.props.iosIcon == undefined) ? <Icon name="ios-home" style={{opacity: 0}} />  : this.renderIcon()}
             </Button>
             <Modal animationType='slide'
                 transparent={false}
                 visible={this.state.modalVisible}
                 onRequestClose={() => {this._setModalVisible(false)}}
                 >
-                <Container>
+                <Container style={this.props.modalStyle}>
                     {this.renderHeader()}
                     <Content>
                         <List dataArray={this.props.children}
                             renderRow={(child) =>
                                 <ListItem style={{paddingVertical: 5}} iconRight button onPress={() => {this._setModalVisible(false);this.props.onValueChange(child.props.value); this.setState({current: child.props.label})}} >
-                                    <Text >{child.props.label}</Text>
+                                    <Text style={[this.getInitialStyle().itemTextStyle, child.props.textStyle]} >{child.props.label}</Text>
                                     {(child.props.value===this.props.selectedValue) ?
-                                        (<Icon name='ios-checkmark-outline' />)
+                                        (<Icon name='ios-checkmark-outline' style={{color: this.props.iconColor}} />)
                                         :
                                         (<Icon name='ios-checkmark-outline' style={{color: 'transparent'}} />)
                                     }
